a28719a567bc2f8a2b7630a18f8ba030e8c7af17
Eduardo Garibay <dark.cloud2007@gmail.com>
2013-01-14 21:44:29 -0600

    Adds spec test suite and compile script.

---
+++ spec/test_suite/hidden_fields.js
@@ -0,0 +1,21 @@
+describe("Hidden fields", function(){
+  var form, fields;
+
+  before(function(){
+    form = '<form id="test-form">\
+      <input id="field1" type="email" name="email" value="foo@bar.com"/>\
+      <input id="field2" type="hidden" name="referrer" value="contact_us"/>\
+    </form>';
+
+    $(form).appendTo('#qunit-fixture');
+    fields = Fields.in('#test-form');
+  });
+
+  it('should reset non hidden fields', function(){
+    fields.clear()
+    console.log(fields.get('email').val());
+    assert(fields.get('referrer').val()).equals('contact_us');
+    assert(fields.get('email').val()).equals('');
+  });
+  
+});
